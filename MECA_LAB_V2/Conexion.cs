cf61e214467cd2275c006c11e6c4ee3a802cc052
Manuel García <the.master.mods@gmail.com>
2020-06-19 20:49:41 -0700

    [NEW] Conexion.cs(Archivo de conexión BD)

---
+++ MECA_LAB_V2/Conexion.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
+
+namespace MECA_LAB_V2
+{
+    class Conexion
+    {
+        private static string host       = "localhost";
+        private static string database   = "mecalab";
+        private static string user       = "root";
+        private static string password   = "";
+
+        private static string cadconn = "Server=" + host + "; Database=" + database + "; Uid=" + user + "; Pwd=" + password + ";";
+
+        public static DataSet MySQL(string sql)
+        {
+            MySqlConnection cnn;
+            MySqlCommand cmd;
+            MySqlDataAdapter da;
+            DataSet ds;
+
+            cnn = new MySqlConnection(cadconn);
+            cmd = new MySqlCommand(sql, cnn);
+            da = new MySqlDataAdapter(cmd);
+            ds = new DataSet();
+
+            try
+            {
+                da.Fill(ds, "tabla");
+            }
+            catch (MySqlException ex)
+            {
+                //MessageBox.Show(ex.Message, "Error No. " + ex.ErrorCode, MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return ds;
+            }
+
+            return ds;
+        }
+    }
+}
