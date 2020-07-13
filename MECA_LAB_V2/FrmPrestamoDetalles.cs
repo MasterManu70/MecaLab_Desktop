d4de7a70e0c9e8885624f6ac209bca94cc90defb
TheSincarS <jesus_fco_981@hotmail.com>
2020-07-13 16:17:19 -0500

    [ADD] FrmPrestamoDetalles - [ADD] icon FrmPrincial

---
+++ MECA_LAB_V2/FrmPrestamoDetalles.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MECA_LAB_V2
+{
+    public partial class FrmPrestamoDetalles : Form
+    {
+        public FrmPrestamoDetalles()
+        {
+            InitializeComponent();
+        }
+        //Formulario Maximiazar Minimizar, Cerrar y Diseño
+        private void btnCerrar_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void btnMinimizar_Click(object sender, EventArgs e)
+        {
+            this.WindowState = FormWindowState.Minimized;
+        }
+    }
+}
